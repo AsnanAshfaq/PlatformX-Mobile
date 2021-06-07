db826ee21208dce157c44fe251ee9e028770f21c
Asnan Ashfaq <65377376+AsnanAshfaq@users.noreply.github.com>
2021-06-07 12:55:05 +0500

    Designed Create Post Screen :rocket:

--- src/Screens/Post/CreatePost.tsx
+++ src/Screens/Post/CreatePost.tsx
@@ -10,49 +10,66 @@ import {
   View,
   TouchableHighlight,
   Keyboard,
+  Image,
   TextInput,
+  ScrollView,
 } from 'react-native';
 import CustomHeader from '../../Components/CustomHeader';
 import CustomDropDown from '../../Components/CustomDropDown';
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import ImagePicker from 'react-native-image-crop-picker';
 import {POST_TYPE} from '../../Constants/sample';
-import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
-import {Sizes, Width} from '../../Constants/Size';
+import {
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from 'react-native-gesture-handler';
+import {Height, Sizes, Width} from '../../Constants/Size';
 import {darkColors} from '../../Constants/Colors';
-import {event} from 'react-native-reanimated';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const ICON_SIZE = Width * 0.07;
+
 type props = {
   navigation: any;
 };
+
 const CreatePost: FC<props> = ({navigation}) => {
   const [text, settext] = useState('');
+  const [textPlacholder, setTextPlacholder] = useState('Write your post . . .');
   const [toggleDropDown, settoggleDropDown] = useState(false);
   const [Selected, setSelected] = useState('Select Post Type');
+  const [Images, setImages] = useState([]);
   const dropDownRef = useRef(null);
   const textInput = useRef(null);
 
   const openGallery = () => {
-    launchImageLibrary(
-      {
-        mediaType: 'photo',
-        maxWidth: 300,
-        maxHeight: 300,
-        selectionLimit: 19,
-      },
-      response => {
-        console.log(response);
-      },
-    );
+    ImagePicker.openPicker({
+      multiple: true,
+    }).then(images => {
+      setImages(prev => [...prev, ...images]);
+    });
   };
   const onViewPressed = () => {
-    console.log(dropDownRef);
-    console.log('Pressed on view');
+    // console.log(dropDownRef);
+    // console.log('Pressed on view');
   };
 
+  const removeImage = (index: number) => {
+    // remove image from setImages
+    setImages(Images.filter((_, i) => i != index));
+  };
+
+  const handlePost = () => {
+    console.log('Clicked on post');
+  };
+
+  // console.log(Images.length);
   useEffect(() => {
     // make text input blur if keyboard is hidden
     Keyboard.addListener('keyboardDidHide', event => {
       if (textInput && textInput.current) {
         textInput?.current?.blur();
+        // and make the placeholder to its previous value
+        setTextPlacholder('Write your post . . .');
       }
     });
     // hide the drop down if the keyboard is open
@@ -64,8 +81,8 @@ const CreatePost: FC<props> = ({navigation}) => {
   }, [textInput]);
 
   useEffect(() => {
-    console.log(dropDownRef.current);
-  }, [dropDownRef]);
+    console.log(Images);
+  }, [Images]);
   return (
     <View onTouchStart={() => console.log('Tocuhed started')}>
       <TouchableWithoutFeedback onPress={() => onViewPressed()}>
@@ -75,7 +92,6 @@ const CreatePost: FC<props> = ({navigation}) => {
           back
           onBackPress={() => navigation.goBack()}
         />
-
         {/* post text view  */}
         <View style={styles.textInputContainer}>
           <TextInput
@@ -84,13 +100,16 @@ const CreatePost: FC<props> = ({navigation}) => {
             onChangeText={settext}
             value={text}
             multiline
-            placeholder={'Write your post...'}
+            placeholder={textPlacholder}
             placeholderTextColor={darkColors.TEXT_COLOR}
-            onFocus={() => console.log('Text input is focused')}
-            onBlur={() => console.log('Text input is blured')}
+            onFocus={() => {
+              if (textInput && textInput.current) {
+                setTextPlacholder('');
+              }
+            }}
+            // onBlur={() => console.log('Text input is blured')}
           />
         </View>
-
         {/* custom drop down  */}
         <CustomDropDown
           data={POST_TYPE}
@@ -99,11 +118,59 @@ const CreatePost: FC<props> = ({navigation}) => {
           Selected={Selected}
           setSelected={setSelected}
         />
+        {/* show image view in a scrollview*/}
 
-        {/* post image view  */}
-        <TouchableHighlight onPress={() => openGallery()}>
-          <Text>Add Image</Text>
-        </TouchableHighlight>
+        {Images.length > 0 && (
+          <ScrollView horizontal>
+            {Images.map((image, index) => {
+              return (
+                <View style={styles.imageContainer} key={index}>
+                  <Image
+                    source={{uri: image?.path}}
+                    style={{
+                      width: image.width > 200 ? 200 : image?.width,
+                      height: image.height > 300 ? 300 : image?.height,
+                      marginHorizontal: 7,
+                      borderRadius: 10,
+                    }}
+                    resizeMode={'cover'}
+                  />
+
+                  <View style={styles.crossContainer}>
+                    <TouchableOpacity onPress={() => removeImage(index)}>
+                      <Ionicons
+                        name={'close-circle-outline'}
+                        size={ICON_SIZE}
+                        color={darkColors.ICON_COLOR}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              );
+            })}
+          </ScrollView>
+        )}
+
+        {/* add  image view  */}
+        <View style={styles.addImageContainer}>
+          <TouchableOpacity onPress={() => openGallery()}>
+            <Ionicons
+              name={'add-circle-outline'}
+              size={ICON_SIZE * 2.7}
+              color={darkColors.SHADOW_COLOR}
+            />
+          </TouchableOpacity>
+          <Text style={styles.addImageText}>Add Image</Text>
+        </View>
+
+        {/* post button view  */}
+        <View style={styles.postButtonContainer}>
+          <TouchableOpacity
+            style={styles.postButton}
+            onPress={() => handlePost()}>
+            <Text style={styles.postButtonText}>Post</Text>
+          </TouchableOpacity>
+        </View>
       </TouchableWithoutFeedback>
     </View>
   );
@@ -127,5 +194,58 @@ const styles = StyleSheet.create({
     borderColor: darkColors.LIGHT_BACKGROUND,
     color: darkColors.TEXT_COLOR,
     fontSize: Sizes.normal * 1.1,
+  },
+  addImageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  addImageText: {
+    color: darkColors.TEXT_COLOR,
+    fontSize: Sizes.normal,
+    fontFamily: 'ComicNeue-Light',
+  },
+  imageContainer: {
+    margin: 0,
+    // flex: 1,
+    height: 300,
+  },
+  image: {
+    width: Width * 0.9,
+    height: Width * (9 / 16),
+  },
+  crossContainer: {
+    position: 'absolute',
+    top: 0,
+    right: 10,
+    bottom: 10,
+  },
+  cross: {
+    color: darkColors.SHADOW_COLOR,
+    fontSize: Sizes.large * 1.2,
+    fontFamily: 'ComicNeue-Bold',
+  },
+  postButtonContainer: {
+    marginHorizontal: Width * 0.04,
+    marginVertical: 5,
+    padding: 5,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  postButton: {
+    backgroundColor: darkColors.SHADOW_COLOR,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: 'transparent',
+    maxHeight: Height * 0.06,
+    width: Width * 0.9,
+    height: Height * 0.06,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  postButtonText: {
+    color: darkColors.TEXT_COLOR,
+    fontSize: Sizes.normal * 1.1,
+    paddingVertical: 2,
   },
 });
