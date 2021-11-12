2a579b3ed2513d0324b2b6f8f783c30ed5cb12a5
Asnan Ashfaq <65377376+AsnanAshfaq@users.noreply.github.com>
2021-11-13 03:36:44 +0500

    Added hackathon result view :tada:

--- src/Screens/Organization/Hackathon/Result/View.tsx
+++ src/Screens/Organization/Hackathon/Result/View.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {FC, useEffect, useState} from 'react';
-import {StyleSheet, Text, View, Image} from 'react-native';
-import {Sizes, Width} from '../../../../Constants/Size';
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import {Height, Sizes, Width} from '../../../../Constants/Size';
 import {useStateValue} from '../../../../Store/StateProvider';
 
 type props = {
@@ -9,16 +10,22 @@ type props = {
 const ViewResult: FC<props> = ({ID}) => {
   const {theme} = useStateValue()[0];
 
+  const handlePress = () => {
+    console.log('Pressed on touchable opacity');
+  };
+
   return (
     <View style={{flex: 1}}>
-      <View style={[styles.container, styles.center]}>
-        <Text style={[styles.heading, {color: theme.TEXT_COLOR}]}>WINNERS</Text>
+      <View style={[styles.marginVertical, styles.center]}>
+        <Text style={[styles.heading, {color: theme.TEXT_COLOR}]}>
+          WINNING PROJECTS
+        </Text>
       </View>
 
-      {/* card  */}
-      <View style={[{flexDirection: 'row', marginVertical: 20}]}>
-        {/* 3rd position  */}
-        <View
+      {/* cards */}
+      <View style={[styles.marginVertical, {flexDirection: 'row'}]}>
+        {/* 3rd position  card*/}
+        <TouchableOpacity
           style={[
             styles.card,
             styles.center,
@@ -33,11 +40,17 @@ const ViewResult: FC<props> = ({ID}) => {
               borderRadius: 10,
               borderTopWidth: 1,
               width: Width * 0.29,
+              height: Height * 0.2,
             },
-          ]}>
+          ]}
+          onPress={() => handlePress()}>
           <Text
             style={[
-              {color: theme.TEXT_COLOR, flexShrink: 1, fontSize: Sizes.normal},
+              {
+                color: theme.TEXT_COLOR,
+                flexShrink: 1,
+                fontSize: Sizes.normal * 0.8,
+              },
             ]}>
             3rd
           </Text>
@@ -46,15 +59,19 @@ const ViewResult: FC<props> = ({ID}) => {
               uri:
                 'https://images.unsplash.com/photo-1572177812156-58036aae439c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvamVjdHN8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
             }}
-            style={styles.projectImage}
+            style={{
+              borderRadius: 40,
+              width: Width * 0.14,
+              height: Width * 0.14,
+            }}
           />
           <Text
-            style={[{color: theme.TEXT_COLOR, fontSize: Sizes.normal * 0.8}]}>
-            Face Recognition Face Recognition
+            style={[{color: theme.TEXT_COLOR, fontSize: Sizes.normal * 0.7}]}>
+            Face Recognition
           </Text>
-        </View>
-        {/* 1st position  */}
-        <View
+        </TouchableOpacity>
+        {/* 1st position card*/}
+        <TouchableOpacity
           style={[
             styles.card,
             styles.center,
@@ -67,8 +84,10 @@ const ViewResult: FC<props> = ({ID}) => {
               borderWidth: 1,
               borderRadius: 10,
               width: Width * 0.41,
+              height: Height * 0.25,
             },
-          ]}>
+          ]}
+          onPress={() => handlePress()}>
           <Text
             style={[{fontSize: Sizes.normal * 1.4, color: theme.TEXT_COLOR}]}>
             1st
@@ -78,14 +97,18 @@ const ViewResult: FC<props> = ({ID}) => {
               uri:
                 'https://images.unsplash.com/photo-1572177812156-58036aae439c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvamVjdHN8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
             }}
-            style={styles.projectImage}
+            style={{
+              borderRadius: 40,
+              width: Width * 0.2,
+              height: Width * 0.2,
+            }}
           />
           <Text style={[{fontSize: Sizes.normal, color: theme.TEXT_COLOR}]}>
-            Face Recognition Face Recognition
+            Chat Bot
           </Text>
-        </View>
-        {/* 2nd position  */}
-        <View
+        </TouchableOpacity>
+        {/* 2nd position card */}
+        <TouchableOpacity
           style={[
             styles.card,
             styles.center,
@@ -99,9 +122,11 @@ const ViewResult: FC<props> = ({ID}) => {
               borderBottomWidth: 1,
               borderRadius: 10,
               borderTopWidth: 1,
-              width: Width * 0.3,
+              width: Width * 0.29,
+              height: Height * 0.2,
             },
-          ]}>
+          ]}
+          onPress={() => handlePress()}>
           <Text style={[{color: theme.TEXT_COLOR, fontSize: Sizes.normal}]}>
             2nd
           </Text>
@@ -110,7 +135,11 @@ const ViewResult: FC<props> = ({ID}) => {
               uri:
                 'https://images.unsplash.com/photo-1572177812156-58036aae439c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvamVjdHN8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
             }}
-            style={styles.projectImage}
+            style={{
+              borderRadius: 40,
+              width: Width * 0.17,
+              height: Width * 0.17,
+            }}
           />
           <Text
             style={[
@@ -119,9 +148,142 @@ const ViewResult: FC<props> = ({ID}) => {
                 fontSize: Sizes.normal * 0.8,
               },
             ]}>
-            Face Recognition Face Recognition
+            Automated System
           </Text>
-        </View>
+        </TouchableOpacity>
+      </View>
+      <View style={[styles.center, {marginTop: Height * 0.25}]}>
+        <Text style={[styles.heading, {color: theme.TEXT_COLOR}]}>WINNERS</Text>
+      </View>
+
+      <View style={[styles.marginVertical]}>
+        {/* 3rd position  card*/}
+        <TouchableOpacity
+          style={[
+            styles.card,
+            styles.center,
+            {
+              borderColor: theme.CARD_BACKGROUND_COLOR,
+              padding: 7,
+              position: 'absolute',
+              marginTop: 10,
+              marginLeft: Width * 0.011,
+              borderLeftWidth: 1,
+              borderBottomWidth: 1,
+              borderRadius: 10,
+              borderTopWidth: 1,
+              width: Width * 0.29,
+              height: Height * 0.2,
+            },
+          ]}
+          onPress={() => handlePress()}>
+          <Text
+            style={[
+              {
+                color: theme.TEXT_COLOR,
+                flexShrink: 1,
+                fontSize: Sizes.normal * 0.8,
+              },
+            ]}>
+            3rd
+          </Text>
+          <Image
+            source={{
+              uri:
+                'https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
+            }}
+            style={{
+              borderRadius: 40,
+              width: Width * 0.14,
+              height: Width * 0.14,
+            }}
+          />
+          <Text
+            style={[{color: theme.TEXT_COLOR, fontSize: Sizes.normal * 0.7}]}>
+            Zain ul Tariq
+          </Text>
+        </TouchableOpacity>
+        {/* 1st position card*/}
+        <TouchableOpacity
+          style={[
+            styles.card,
+            styles.center,
+            {
+              borderColor: theme.CARD_BACKGROUND_COLOR,
+              //   padding: 20,
+              paddingVertical: 20,
+              position: 'absolute',
+              marginLeft: Width * 0.295,
+              borderWidth: 1,
+              borderRadius: 10,
+              width: Width * 0.41,
+              height: Height * 0.25,
+            },
+          ]}
+          onPress={() => handlePress()}>
+          <Text
+            style={[{fontSize: Sizes.normal * 1.4, color: theme.TEXT_COLOR}]}>
+            1st
+          </Text>
+          <Image
+            source={{
+              uri:
+                'https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg',
+            }}
+            style={{
+              borderRadius: 40,
+              width: Width * 0.2,
+              height: Width * 0.2,
+            }}
+          />
+          <Text style={[{fontSize: Sizes.normal, color: theme.TEXT_COLOR}]}>
+            Saqib Zaman
+          </Text>
+        </TouchableOpacity>
+        {/* 2nd position card */}
+        <TouchableOpacity
+          style={[
+            styles.card,
+            styles.center,
+            {
+              borderColor: theme.CARD_BACKGROUND_COLOR,
+              padding: 7,
+              position: 'absolute',
+              marginTop: 10,
+              marginLeft: Width * 0.7,
+              borderRightWidth: 1,
+              borderBottomWidth: 1,
+              borderRadius: 10,
+              borderTopWidth: 1,
+              width: Width * 0.29,
+              height: Height * 0.2,
+            },
+          ]}
+          onPress={() => handlePress()}>
+          <Text style={[{color: theme.TEXT_COLOR, fontSize: Sizes.normal}]}>
+            2nd
+          </Text>
+          <Image
+            source={{
+              uri:
+                'https://media.istockphoto.com/photos/learn-to-love-yourself-first-picture-id1291208214?b=1&k=20&m=1291208214&s=170667a&w=0&h=sAq9SonSuefj3d4WKy4KzJvUiLERXge9VgZO-oqKUOo=',
+            }}
+            style={{
+              borderRadius: 40,
+              width: Width * 0.17,
+              height: Width * 0.17,
+            }}
+          />
+          <Text
+            style={[
+              {
+                color: theme.TEXT_COLOR,
+                fontSize: Sizes.normal * 0.8,
+              },
+            ]}>
+            Saif Waheed Raja
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -134,7 +296,7 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   heading: {
-    fontSize: Sizes.normal * 1.2,
+    fontSize: Sizes.normal * 1.25,
   },
   smallText: {
     fontSize: Sizes.normal * 0.66,
@@ -145,6 +307,9 @@ const styles = StyleSheet.create({
   },
   margin: {
     marginHorizontal: Width * 0.04,
+  },
+  marginVertical: {
+    marginVertical: Height * 0.028,
   },
   projectImage: {
     borderRadius: 40,
